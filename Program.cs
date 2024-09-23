9b86a865c3139daf2a87259fb9e01a09b77b24af
Sebastian Drela <sebastian.drela@wp.pl>
2024-09-23 03:04:58 +0200

    fix space

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ partial class Program
         var mcWriter = new MachineCodeBuilder<MyUnmanagedDelegate>();        
         mcWriter.AddInstruction([0x48, 0x83, 0xEC, 0x28],       "sub rsp, 0x28", "Align stack (16-byte alignment)");       
         mcWriter.SetRax<MyManagedDelegate>(MyMagicManagedMethod);
-        mcWriter.AddInstruction([0xB9, 0x05, 0x00, 0x00, 0x00], "mov ecx, 0x05" , "Move to ecx 5");
+        mcWriter.AddInstruction([0xB9, 0x05, 0x00, 0x00, 0x00], "mov ecx, 0x05", "Move to ecx 5");
         mcWriter.AddInstruction([0xBA, 0x0A, 0x00, 0x00, 0x00], "mov edx, 0x0A", "Move to edx 10");
         mcWriter.AddInstruction([0xFF, 0xD0],                   "call rax"     , "Call the function pointer in RAX");
         mcWriter.AddInstruction([0x83, 0xC0, 0x64],             "add eax, 0x64", "Add eax 100");
